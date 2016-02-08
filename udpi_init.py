d13bee1fe9843067a69e958020e3d1639d68a90d
alb <alberto1337@gmail.com>
2016-02-08 11:07:42 +0100

    added libraries and main method

--- udpi_init.py
+++ udpi_init.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
+import socket
+from struct import *
+import datetime
+import pcapy
+import sys
 
-print "test"
+def main(argv):
+    #list all devices
+    devices = pcapy.findalldevs()
+    print devices
+
+
+
+
+
+if __name__ == "__main__":
+  main(sys.argv)
